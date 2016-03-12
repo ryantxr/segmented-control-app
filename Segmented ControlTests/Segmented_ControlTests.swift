37360bd3f01c1fb9940a16145d1a72f8f49ad92f
ryantxr <ryantxr@gmail.com>
2016-03-12 10:54:44 -0500

    Initial Commit

---
+++ Segmented ControlTests/Segmented_ControlTests.swift
@@ -0,0 +1,36 @@
+//
+//  Segmented_ControlTests.swift
+//  Segmented ControlTests
+//
+//  Created by ryan teixeira on 3/12/16.
+//  Copyright © 2016 Ryan Teixeira. All rights reserved.
+//
+
+import XCTest
+@testable import Segmented_Control
+
+class Segmented_ControlTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
